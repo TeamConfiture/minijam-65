f2516b997e0e76ee02f888ad7ce0f5cead4d1fc4
Kimi <katran@etud.insa-toulouse.fr>
2020-10-17 00:09:27 +0200

    Add skeleton

---
+++ Choupy Scary/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Game Attributes")]
+    public bool blocPosition = false;
+    [Header("Game Status")]
+    public bool[] obtainedStatus = new bool[3] {false, false, false};
+
+    public static GameManager Instance;
+
+    public bool Interacting = false;
+
+    public GameObject respawnLocation;
+    public GameObject respawnCamera;
+
+    public delegate void ResetAction();
+    public static event ResetAction ResetPrefabs;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+
+    public void ResetGame()
+    {
+        Interacting = false;
+        blocPosition = false;
+        obtainedStatus[0]=false;
+        obtainedStatus[1]=false;
+        obtainedStatus[2]=false;
+    }
+
+    public void GetArtifact(int artifactId) {
+        Debug.Log("Obtained Artifact !");
+        if (artifactId < 1 || artifactId > 3) {
+            Debug.Log("Wrong ID !!!");
+        } else {
+            obtainedStatus[artifactId-1] = true;
+        }
+    }
+
+    public bool GetArtifactValue(int artifactId) {
+        if (artifactId < 1 || artifactId > 3)
+            return false;
+        return obtainedStatus[artifactId-1];
+    }
+
+    public void TriggerPrefabsReset()
+    {
+        ResetPrefabs();
+    }
+}
