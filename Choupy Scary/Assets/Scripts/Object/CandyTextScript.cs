0d5d3b24d9afa38facc961a7817f30d0a49d9ffc
Kimi <katran@etud.insa-toulouse.fr>
2020-10-17 15:44:14 +0200

    add candy

---
+++ Choupy Scary/Assets/Scripts/Object/CandyTextScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CandyTextScript : MonoBehaviour
+{
+    Text text;
+    public static int candyAmount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (candyAmount > 0)
+        {
+            text.text = "Candy: " + candyAmount;
+        } else
+        {
+            text.text = "Out of candy!";
+        }
+    }
+}
