0cd79fea05468689113baf9d3374e20df44c8941
DoctorTee <14941050+doctortee@users.noreply.github.com>
2020-10-17 12:21:32 +0200

    Added a new character prefab

---
+++ Choupy Scary/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [Header("Attributes")]
+    public float moveMultiplier = 7f;
+
+    GameManager manager = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        Debug.Log(movement);
+        transform.position += movement * Time.deltaTime * moveMultiplier;
+    }
+}
